3c81a8bf45db68b93d76e364e5822058eb6edad9
FLATISHKA <60339162+FLATISHKA@users.noreply.github.com>
2020-08-24 14:03:39 +0300

    python

--- osa1.py
+++ osa1.py
@@ -36,3 +36,28 @@ for i in range(1, 1000):
     n = str(random.randint(1,100)) + "," + str(random.randint(1,100))
     sijaintiLista.append(n)
 print(sijaintiLista)
+
+# Listan järjestäminen
+lista = [1, 3, 5, 4, 8, 2, 9, 6, 7]
+lista.sort()
+print(lista)
+
+# Pistelista
+kierrokset = True
+x=[]
+y=[]
+lisaaInput = ""
+while kierrokset:
+    lisaaInput = input("Anna pelaaja: ")
+    if lisaaInput == "lopeta":
+        kierrokset = False
+    else:
+        x.append(lisaaInput)
+        y.append(input("Anna pisteet: "))
+
+esiintymat = [i for i, z in enumerate(y) if z == max (y)]
+pituusEsiintymat=len(esiintymat)
+tulokset = 0
+while(tulokset < pituusEsiintymat):
+    print(x[esiintymat[tulokset]] + ", " + y[esiintymat[tulokset]])
+    tulokset += 1
